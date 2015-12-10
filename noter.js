3a7bcd54c0b9315475726052c4f5acdd685d8d3e
Wells Johnston <wellsjohnston@gmail.com>
2015-12-09 23:32:52 -0500

    first commit

---
+++ noter.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var args = process.argv.slice(2);
+var notersDir = `${process.env.HOME}/.noters`;
+
+
+var location = `${notersDir}/${process.cwd().replace(/\//g, '-')}`;
+
+if (args[0] == 'clear') {
+  if (fs.existsSync(location)) {
+    fs.unlinkSync(location);
+    console.log('\nnote removed.')
+  } else {
+    console.log('\npshhhhhhh there isn\'t anything here.');
+  }
+} else if (args.length > 0) {
+  if (!fs.existsSync(notersDir)) {
+    fs.mkdirSync(notersDir);
+  }
+  fs.writeFileSync(location, args.join(' '));
+  console.log('\nnote saved :)');
+} else {
+  if (fs.existsSync(location)) {
+    console.log('\n' + fs.readFileSync(location).toString());
+  } else {
+    console.log('\nNo notes found for this directory.');
+  }
+}
