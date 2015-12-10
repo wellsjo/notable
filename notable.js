b96652b4199b1c4dfa396aab79fb8c651849474d
Wells Johnston <wellsjohnston@gmail.com>
2015-12-10 04:58:26 -0500

    add dates to messages

--- notable.js
+++ notable.js
@@ -4,22 +4,25 @@ var fs = require('fs');
 var args = process.argv.slice(2);
 var notersDir = `${process.env.HOME}/.noters`;
 var location = `${notersDir}/${process.cwd().replace(/\//g, '-')}`;
+var now = new Date();
+var parts = now.toString().split(' ');
+var date = `${parts[0]}, ${parts[1]} ${parts[[2]]} ${parts[3]} ${parts[4]}`;
 
 if (args[0] == 'clear') {
   if (fs.existsSync(location)) {
     fs.unlinkSync(location);
-    console.log('\naote removed')
+    console.log('note removed')
   }
 } else if (args.length > 0) {
   if (!fs.existsSync(notersDir)) {
     fs.mkdirSync(notersDir);
   }
-  fs.writeFileSync(location, args.join(' '));
-  console.log('\nnote saved');
+  fs.writeFileSync(location, `${date}\n${args.join(' ')}`);
+  console.log('note saved');
 } else {
   if (fs.existsSync(location)) {
-    console.log('\n' + fs.readFileSync(location).toString());
+    console.log(fs.readFileSync(location).toString());
   } else {
-    console.log('\nnothng here');
+    console.log('nothing here');
   }
 }
